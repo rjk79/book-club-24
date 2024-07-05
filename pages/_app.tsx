7b052365ca692b5823ba9e2568818814d17a5464
rjk79 <robertku79@gmail.com>
2024-07-05 16:54:52 -0400

    Add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,16 +5,18 @@ import {
 } from '@tanstack/react-query'
 import Layout from '../components/layout/layout'
 import { SessionProvider } from "next-auth/react"
+import { Analytics } from "@vercel/analytics/react"
 
 const queryClient = new QueryClient()
 
 function MyApp({ Component, pageProps: { session, ...pageProps }, }) {
   return (
     <QueryClientProvider client={queryClient}>
-        <SessionProvider session={session}>
+      <SessionProvider session={session}>
         <Layout>
-      <Component {...pageProps} />
-      </Layout>
+          <Component {...pageProps} />
+          <Analytics />
+        </Layout>
       </SessionProvider>
     </QueryClientProvider>
 
